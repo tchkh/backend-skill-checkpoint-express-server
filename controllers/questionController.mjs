07c7871589af634729e6a553ecccf4d2ef8b4040
jayOnTheStreet <taychakhun.r@gmail.com>
2025-04-11 11:33:31 +0700

    feat: add question api base requirement

---
+++ controllers/questionController.mjs
@@ -0,0 +1,107 @@
+import connectionPool from '../utils/db.mjs'
+
+export const getAllquestions = async (req, res) => {
+  try {
+    const result = await connectionPool.query('select * from questions')
+    return res.status(200).json({ data: result.rows })
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to fetch questions.' })
+  }
+}
+
+export const getQuestionById = async (req, res) => {
+  try {
+    const isIdExists = await connectionPool.query({
+      text: 'select exists (select * from questions where id = $1)',
+      values: [req.params.id],
+    })
+
+    if (!isIdExists.rows[0].exists) {
+      return res.status(404).json({ message: 'Question not found.' })
+    }
+
+    const result = await connectionPool.query({
+      text: 'select * from questions where id = $1',
+      values: [req.params.id],
+    })
+    return res.status(200).json({ data: result.rows[0] })
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to fetch questions.' })
+  }
+}
+
+export const createQuestion = async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.description || !req.body.category) {
+      return res.status(400).json({ message: 'Invalid question data.' })
+    }
+    const result = connectionPool.query({
+      text: `insert into questions (title, description, category) 
+      values ($1, $2, $3)
+      returning *`,
+      values: [req.body.title, req.body.description, req.body.category],
+    })
+    return res.status(201).json({ message: 'Question created successfully.' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to fetch questions.' })
+  }
+}
+
+export const updateQuestion = async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.description || !req.body.category) {
+      return res.status(400).json({ message: 'Invalid question data.' })
+    }
+
+    const isIdExists = await connectionPool.query({
+      text: 'select exists (select * from questions where id = $1)',
+      values: [req.params.id],
+    })
+
+    if (!isIdExists.rows[0].exists) {
+      return res.status(404).json({ message: 'Question not found.' })
+    }
+
+    const result = await connectionPool.query({
+      text: `update questions set title = $1, description = $2, category = $3
+      where id = $4 returning *`,
+      values: [
+        req.body.title,
+        req.body.description,
+        req.body.category,
+        req.params.id,
+      ],
+    })
+    return res.status(200).json({ message: 'Question updated successfully.' })
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to fetch questions.' })
+  }
+}
+
+export const deleteQuestion = async (req, res) => {
+  try {
+    const isIdExists = await connectionPool.query({
+      text: 'select exists (select * from questions where id = $1)',
+      values: [req.params.id],
+    })
+
+    if (!isIdExists.rows[0].exists) {
+      return res.status(404).json({ message: 'Question not found.' })
+    }
+
+    const result = await connectionPool.query({
+      text: `
+      delete from questions where id = $1
+      returning *`,
+      values: [req.params.id],
+    })
+
+    if (result.rowCount > 0) {
+      return res
+        .status(200)
+        .json({ message: 'Question post has been deleted successfully.' })
+    }
+  } catch (error) {
+    return res.status(500).json({ message: 'Unable to fetch questions.' })
+  }
+}
---
+++ routes/questionRoute.mjs
@@ -0,0 +1,19 @@
+import { Router } from 'express'
+import {
+  createQuestion,
+  deleteQuestion,
+  getAllquestions,
+  getQuestionById,
+  getQuestionBySearch,
+  updateQuestion,
+} from '../controllers/questionController.mjs'
+
+const questionRouter = Router()
+
+questionRouter.get('/', getAllquestions)
+questionRouter.get('/:id', getQuestionById)
+questionRouter.post('/', createQuestion)
+questionRouter.put('/:id', updateQuestion)
+questionRouter.delete('/:id', deleteQuestion)
+
+export default questionRouter
